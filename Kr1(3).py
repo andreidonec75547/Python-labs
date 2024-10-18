de2e302ff7bfaed116b8218ea4e577f5bc2b9f5a
Rick <96939804+andreidonec75547@users.noreply.github.com>
2024-10-18 18:23:55 +0300

    Update Kr1(3).py

--- Kr1(3).py
+++ Kr1(3).py
@@ -1,4 +1,34 @@
 
+# Контрольная работа 1, Лабораторная работа 3, задание 3 уровня, номер 8
+
+
+import random
+Mol = [random.randint(-(10**10), 10**10) for _ in range(8)]
+print(Mol)
+
+kol = list(filter(lambda x: x < 0, Mol))
+kol.sort(reverse=True)
+print(kol)
+
+def get_ihd(lon, ran):
+    indexin = []
+    for index, element in enumerate(lon):
+        if element in ran:
+            indexin.append(index)
+    return indexin
+
+result = get_ihd(Mol, kol)
+print(result)
+
+doom = 0
+for n in result:
+    Mol[n] = kol[doom]
+    doom = doom + 1
+
+print(Mol)
+
+
+
 # Контрольная работа 1, Лабораторная работа 3, задание 1 уровня, номер 6
 
 Vector = [1, 2, 3, 4, 5]
