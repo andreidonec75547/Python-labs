f1be11b3c8df029223f17fb1237d44c1eff49ff2
Rick <96939804+andreidonec75547@users.noreply.github.com>
2024-10-18 16:14:01 +0300

    Update Kr1(1).py

--- Kr1(1).py
+++ Kr1(1).py
@@ -1,3 +1,26 @@
+
+# Контрольная работа 1, Лабораторная работа 1, задание 1 уровня, номер 8
+
+spot = [1, 2, 3, 4, 5, 6]
+goh = []
+sumin = 0
+
+for num in spot:
+    factorial = 1
+
+    while num > 1:
+        factorial = factorial * num
+        num = num - 1
+
+    goh.append(factorial)
+
+for col in goh:
+    sumin += col
+    
+print(sumin)
+
+
+
 # Контрольная работа 1, Лабораторная работа 1, задание 2 уровня, номер 5
 a = 13
 i = a
