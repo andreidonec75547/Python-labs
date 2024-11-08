3d3101f1a34aabfb7b7af94d158e89a230b62daf
Rick <96939804+andreidonec75547@users.noreply.github.com>
2024-11-08 15:45:18 +0300

    Update lab4(1).py

--- lab4(1).py
+++ lab4(1).py
@@ -1,7 +1,19 @@
-matr = [[8, 9, 3, 1, 6], [-6, -45, 7, 2, 21], [56, 83, -25, -89, 54], [90, 37, -15, 32, 72], [-10, -40, 72, 71, -86],
-        [3, 8, 75, 14, 9]]
+matr = [[8, -9, 3, -1, 6], [-6, -45, 7, 2, 21], [56, 83, -25, -89, 54], [90, 37, -15, 32, 72], [-10, -40, 72, 71, -86],
+        [3, -8, 75, -14, 9]]
+print(matr)
+
+wat = []
 
 for i in matr:
-        for elem in i:
-                print(elem, end=" ")
-        print()
+    kol = list(filter(lambda x: x < 0, i))
+
+    v = len(kol)
+    wat.append(v)
+
+max_v = max(wat)
+min_v = min(wat)
+
+a = wat.index(max_v)
+b = wat.index(min_v)
+matr[a], matr[b] = matr[b], matr[a]
+print(matr)
