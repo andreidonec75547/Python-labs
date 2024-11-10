3c53a6a7fd2b9bee9302a4f5d1369c42db702664
Rick <96939804+andreidonec75547@users.noreply.github.com>
2024-11-11 00:55:47 +0300

    Create Lab4(2).py

---
+++ Lab4(2).py
@@ -0,0 +1,30 @@
+matr = [[1, 4, 9, 2, 65, 39, 6], [76, 21, 84, 5, 7, 24, 8], [8, 3, 23, 12, 53, 41, 41], [49, 85, 17, 32, 75, 61, 53], [79, 49, 72, 62, 44, 82, 35]]
+
+matr_end = []
+
+for i in matr:
+    kol = list(filter(lambda x: x > 0, i))
+
+    max_v = max(kol)
+
+    a = kol.index(max_v)
+
+    mi = (a - 1)
+    ma = a + 1
+
+    ln = kol[ma]
+
+    gn = kol[mi]
+
+    if ln > gn:
+        gn *= 2
+    else:
+        ln *= 2
+
+    kol[ma] = ln
+    kol[mi] = gn
+
+    matr_end.append(kol)
+
+print(matr)
+print(matr_end)
