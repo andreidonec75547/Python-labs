884a56dfb4a24984c8a345b4c726df84cc0054f7
Rick <96939804+andreidonec75547@users.noreply.github.com>
2024-12-28 01:15:04 +0300

    Update lab5(3).py

--- lab5(3).py
+++ lab5(3).py
@@ -3,10 +3,25 @@
 
 matr = [[1, 2, 3, 4], [5, 2, 8, 9], [6, 3, 12, 1]]
 
+def sort_by(a_list, selector, ton):
+    return sorted(a_list, key=selector, reverse=ton)
+
+m = []
+
 for i in matr[1::2]:
-    i.sort()
+    for ch in i:
+        m.append(ch)
+
+n = sort_by(m, None, False)
+
+matr[1:2] = [n]
+
+s = []
 
 for a in matr[::2]:
-    a.sort(reverse=True)
+    s.append(sort_by(a, None, True))
+
+matr[0] = s[0]
+matr[2] = s[1]
 
 print(matr)
