7478e8828b68f77df3ffacc77807db18befb13cf
Rick <96939804+andreidonec75547@users.noreply.github.com>
2024-12-30 14:46:42 +0300

    Update lab6(1).py

--- lab6(1).py
+++ lab6(1).py
@@ -1,4 +1,4 @@
-#Лабораторная работа 6, задание 1 уровня, номер 3
+# Лабораторная работа 6, задание 1 уровня, номер 3
 
 class test:
     name = None
@@ -8,13 +8,14 @@ class test:
         self.name = name
         self.col = col
 
+
 k1 = test()
 k1.uno_test("Эйнштейн", 52)
 
 k2 = test()
 k2.uno_test("Бор", 35)
 
-k3= test()
+k3 = test()
 k3.uno_test("Планк", 37)
 
 k4 = test()
@@ -40,7 +41,6 @@ k10.uno_test("неизвестный", 22)
 
 golos = [k1.col, k2.col, k3.col, k4.col, k5.col, k6.col, k7.col, k8.col, k9.col, k10.col]
 golos.sort(reverse=True)
-print(golos)
 
 dop = [k1.col, k2.col, k3.col, k4.col, k5.col, k6.col, k7.col, k8.col, k9.col, k10.col]
 name = [k1.name, k2.name, k3.name, k4.name, k5.name, k6.name, k7.name, k8.name, k9.name, k10.name]
@@ -48,12 +48,12 @@ name = [k1.name, k2.name, k3.name, k4.name, k5.name, k6.name, k7.name, k8.name, 
 totol = 0
 
 for num in dop:
-     totol += num
+    totol += num
 
+print("Пять часто встречаюшихся ответов и их процент относительно остальных ответов:")
 for g in golos:
     if g >= golos[4]:
         for prov in dop:
             if prov == g:
                 i = dop.index(prov)
                 print(name[i], "процент голосов:", round(dop[i] / totol * 100), "%")
-              
