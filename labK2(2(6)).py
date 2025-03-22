8b1b56962e042e6dc9426533ce85ffcd1cb0eba5
Rick <96939804+andreidonec75547@users.noreply.github.com>
2025-03-22 14:21:55 +0300

    Update labK2(2(6)).py

--- labK2(2(6)).py
+++ labK2(2(6)).py
@@ -2,36 +2,33 @@
 # x = [270, 327, 402, 477, 627, 777, 927, 1077, 1227, 1527, 1827]
 # y = [10, 7.5, 6, 5.1, 3.5, 3.1, 5, 8, 10, 2.5, 2]
 
-file = open('x.txt', 'w')
+file = open('xy.txt', 'w')
 
-file.write('270 327 402 477 627 777 927 1077 1227 1527 1827')
-
-file.close()
-
-file = open('y.txt', 'w')
-
+file.write('270 327 402 477 627 777 927 1077 1227 1527 1827 \n')
 file.write('10 7.5 6 5.1 3.5 3.1 5 8 10 2.5 2')
 
 file.close()
 
 x = []
-pon = open('x.txt', 'r')
+pon = open('xy.txt', 'r')
 
 lol = (pon.read())
-word_l = lol.split()
+word_l = lol.split("\n")
+lon = word_l[0].split()
 
-for word in word_l:
+for word in lon:
     x.append(int(word))
 
 pon.close()
 
 y = []
-pon = open('y.txt', 'r')
+pon = open('xy.txt', 'r')
 
 lol = (pon.read())
-word_l = lol.split()
+word_l = lol.split("\n")
+lon = word_l[1].split()
 
-for word in word_l:
+for word in lon:
     y.append(float(word))
 
 pon.close()
