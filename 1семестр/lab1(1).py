1b3da478b220d346bafea8763d7ae020af38e633
Rick <96939804+andreidonec75547@users.noreply.github.com>
2025-03-29 14:37:58 +0300

    Update lab1(1).py

--- 1семестр/lab1(1).py
+++ 1семестр/lab1(1).py
@@ -1,1 +1,13 @@
 
+#Лабораторная работа 1, задание 1 уровня, номер 1 
+
+numms =[]
+i = 2
+total = 0
+while i <= 35:
+    numms.append(i)
+    i += 3
+
+for num in numms:
+     total += num
+print(total)
