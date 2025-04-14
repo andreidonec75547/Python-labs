569a82c34453aa312ded11f2e2c4ce43f4cfd664
Rick <96939804+andreidonec75547@users.noreply.github.com>
2025-04-14 10:31:14 +0300

    Update k3-n.py

--- 2семестр/методКсоседий/k3-n.py
+++ 2семестр/методКсоседий/k3-n.py
@@ -185,6 +185,115 @@ else:
 
 print(bl_t_x)
 
+
+#через цикл перебирается значения по y насколько эта точка удалена от остальных по y
+ny = []
+
+for i in y:
+    ly = i - y_1
+    ny.append(ly)
+
+#сортируем масив
+ky = sorted(ny)
+
+#закидываем в масив только отрецательные элементы
+by = []
+
+for i in ky:
+    if i < 0:
+     by.append(i)
+
+#все элементы делаем положительными
+sy = []
+
+for i in by:
+    gy = i * -1
+    sy.append(gy)
+
+#находим минимальный элемент
+min_1y = min(sy)
+
+#убераем уже найденый минимальный элемет из масива
+sy_2 = []
+
+for i in sy:
+    if i > min_1y:
+        sy_2.append(i)
+
+#находим ещё один минимальный элемент из масива y
+min_1_1y = min(sy_2)
+
+#закидываем в масив только положительные элементы
+dy = []
+
+for i in ky:
+    if i > 0:
+        dy.append(i)
+
+#находим минимальный элемент из положительных
+min_2y = min(dy)
+
+bl_t_y = []
+
+#смотрим растояние по y
+if min_2y <= min_1y:
+    if min_2y < min_1_1y:
+        for i in ky:
+            if min_2y == i:
+                tchk3y = y_1 + min_2y
+
+        index3y = -1
+        for i in xy:
+            index3y += 1
+            rs3y = i[1]
+            if tchk3y == rs3y:
+                pol_tchk3y = xy[index3y]
+                bl_t_y.append(pol_tchk3y)
+    else:
+        tochka1_1y = min_1_1y * (-1)
+
+        for i in ky:
+            if tochka1_1y == i:
+                tchk2y = y_1 + tochka1_1y
+
+        index2y = -1
+        for i in xy:
+            index2y += 1
+            rs2y = i[0]
+            if tchk2y == rs2y:
+                pol_tchk2y = xy[index2y]
+                bl_t_y.append(pol_tchk2y)
+else:
+    tochka1y = min_1y * (-1)
+
+    for i in ky:
+        if tochka1y == i:
+            tchk1y = y_1 + tochka1y
+
+    index1y = -1
+    for i in xy:
+        index1y += 1
+        rs1y = i[0]
+        if tchk1y == rs1y:
+            pol_tchk1y = xy[index1y]
+            bl_t_y.append(pol_tchk1y)
+
+print(bl_t_y)
+
+print(x_1, y_1)
+
+x1_1 = []
+
+for i in bl_t_x:
+    x1_1.append(i[0])
+print(x1_1)
+#убераем уже найденые точки из масива
+x1 = []
+
+for i in xy:
+    if i != bl_t_x:
+        x1.append(i)
+print(x1)
 #строим график для наглядности и проверки
 # import matplotlib.pyplot as plt
 # plt.scatter(x, y)
