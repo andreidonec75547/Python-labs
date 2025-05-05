8eeedadc40152422804066a2a18098892cc54093
Rick <96939804+andreidonec75547@users.noreply.github.com>
2025-05-05 09:30:47 +0300

    Update regrs.py

--- 2семестр/Регресия/regrs.py
+++ 2семестр/Регресия/regrs.py
@@ -2,8 +2,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-area = np.array([28, 42, 45, 52, 56, 68, 70, 75, 90]).reshape(-1, 1)
-price = np.array([3.1, 3.8, 3.9, 4.4, 4.5, 5.9, 5.6, 6.4, 7.3]).reshape(-1, 1)
+file = open('dann.txt', 'w')
+
+file.write('28 42 45 52 56 68 70 75 90 \n')
+file.write('3.1 3.8 3.9 4.4 4.5 5.9 5.6 6.4 7.3')
+
+file.close()
+
+are1 = []
+pon = open('dann.txt', 'r')
+
+lol = (pon.read())
+word_l = lol.split("\n")
+lon = word_l[0].split()
+
+for word in lon:
+    are1.append(int(word))
+
+pon.close()
+
+pric1 = []
+pon = open('dann.txt', 'r')
+
+lol = (pon.read())
+word_l = lol.split("\n")
+lon = word_l[1].split()
+
+for word in lon:
+    pric1.append(float(word))
+
+pon.close()
+
+area = np.array(are1).reshape(-1, 1)
+price = np.array(pric1).reshape(-1, 1)
 
 model = LinearRegression()
 model.fit(area, price)
